1a53405099601703a08c7114a90bf61c1b64e9a0
Klaus Hartl <klaus@door2door.io>
2017-02-09 16:10:01 +0100

    Add initial implementation

--- README.md
+++ README.md
@@ -1,1 +1,24 @@
-# wercker-bugsnag-deploy-tracking-step
+# Wercker Bugsnag Deploy Tracking After Step
+
+A Wercker deploy after step that notifies Bugsnag about any new deploys or releases of your application.
+
+Using Bugsnag's deploy tracking API: https://docs.bugsnag.com/api/deploy-tracking/
+
+## Options
+
+* `api_key` The Bugsnag API key
+* `stages` Comma separated list of stages applicable for deployment tracking
+
+The step requires a `STAGE` environment variable to be present during the execution of the deploy pipeline,
+in order to pass this on to the Bugsnag api as the current release stage. This variable should resemble
+the release stages that may also be in use for error tracking.
+
+## Example
+
+```
+deploy:
+    after-steps:
+        - bugsnag-deploy-tracking-notify:
+            api_key: $BUGSNAG_API_KEY
+            stages: "production"
+```
---
+++ run.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+
+set -e
+
+ERROR_COLOR=$(tput setaf 1 ; tput bold)
+
+function _message() {
+    msg=$1
+    color=$2
+    echo -e "${color}${msg}${RESET_COLOR}"
+}
+
+function fail() {
+    _message "failed: ${1}" $ERROR_COLOR
+    echo "${1}" > "$WERCKER_REPORT_MESSAGE_FILE"
+    exit 1
+}
+
+# Check if API key is present
+if [ -z "$WERCKER_BUGSNAG_DEPLOY_TRACKING_NOTIFY_API_KEY" ]; then
+  fail "Please provide a Bugsnag API key."
+fi
+
+# Skip when deploy failed (use as an deploy after step)
+if [ "$WERCKER_RESULT" = "failed" ]; then
+  return 0
+fi
+
+# Skip when stage not applicable for deploy tracking
+stage_applicable=0;
+IFS=,; for x in $WERCKER_BUGSNAG_DEPLOY_TRACKING_NOTIFY_STAGES; do
+  # Don't forget to trim space, just in case the list goes like " foo, bar" etc.
+  if [ "${x//[[:space:]]/}" == "$STAGE" ]; then
+    stage_applicable=1
+  fi
+done
+if [[ $stage_applicable == 0 ]]; then
+  return 0
+fi
+export BUGSNAG_DEPLOY_TRACKING_API_URL="https://notify.bugsnag.com/deploy"
+
+payload="apiKey=$WERCKER_BUGSNAG_DEPLOY_TRACKING_NOTIFY_API_KEY"
+payload=$payload"&releaseStage=$STAGE"
+payload=$payload"&repository=$WERCKER_GIT_REPOSITORY"
+payload=$payload"&branch=$WERCKER_GIT_BRANCH"
+payload=$payload"&revision=$WERCKER_GIT_COMMIT"
+
+# Post the result
+RESULT=$(curl -d "$payload" -s "$BUGSNAG_DEPLOY_TRACKING_API_URL" --output "$WERCKER_STEP_TEMP"/result.txt -w "%{http_code}")
+cat "$WERCKER_STEP_TEMP/result.txt"
+
+if [ "$RESULT" = "400" ]; then
+  fail "There was an error reading the payload."
+fi
